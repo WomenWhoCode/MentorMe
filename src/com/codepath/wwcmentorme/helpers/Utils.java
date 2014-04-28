31fb75d2fa759513bfd1144ce102e54ead26478e
Nidhi Shah <nidhi.1608@gmail.com>
2014-04-27 17:39:48 -0700

    Geocoding and Reverse Geocoding

--- src/com/codepath/wwcmentorme/helpers/Utils.java
+++ src/com/codepath/wwcmentorme/helpers/Utils.java
@@ -1,19 +1,27 @@
 package com.codepath.wwcmentorme.helpers;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.http.AndroidHttpClient;
 import android.util.Log;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -135,5 +143,127 @@ public class Utils {
 		}
 		container.add(insertionIndex, object);
 		return insertionIndex;
+	}
+	
+	private static Geocoder sGeocoder = null;
+	
+	public static class LocationParams {
+		double lat; double lng;
+		String address;
+		
+		public LocationParams(double lat, double lng) {
+			this.lat = lat; this.lng = lng;
+		}
+		
+		public LocationParams(final String address) {
+			this.address = address;
+		}
+	}
+	
+	public static LocationParams DEFAULT_LOCATION = new LocationParams("San Francisco, CA");
+	
+	private static Address getLocationInfo(String address) {
+		JSONObject jsonObject = null;
+		String query = "http://maps.google.com/maps/api/geocode/json?address=" + address.replaceAll(" ","%20")
+				+ "&sensor=false";
+		Address addr = null;
+		HttpClient client = AndroidHttpClient.newInstance("ReverseGeocoder");
+		HttpGet httpGet = new HttpGet(query);
+
+		HttpResponse response;
+		StringBuilder stringBuilder = new StringBuilder();
+
+		try {
+			response = client.execute(httpGet);
+			if (response.getStatusLine().getStatusCode() == 200) {
+
+				HttpEntity entity = response.getEntity();
+				InputStream stream = entity.getContent();
+				int b;
+				while ((b = stream.read()) != -1) {
+					stringBuilder.append((char) b);
+				}
+				try {
+					jsonObject = new JSONObject(stringBuilder.toString());
+					addr = new Address(Locale.getDefault());
+					JSONArray addrComp = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+							.getJSONArray("address_components");
+					String locality = ((JSONArray)((JSONObject)addrComp.get(0)).get("types")).getString(0);
+					if (locality.compareTo("locality") == 0) {
+						locality = ((JSONObject)addrComp.get(0)).getString("long_name");
+						addr.setLocality(locality);
+					}
+					String adminArea = ((JSONArray)((JSONObject)addrComp.get(2)).get("types")).getString(0);
+					if (adminArea.compareTo("administrative_area_level_1") == 0) {
+						adminArea = ((JSONObject)addrComp.get(2)).getString("long_name");
+						addr.setAdminArea(adminArea);
+					}
+					String country = ((JSONArray)((JSONObject)addrComp.get(3)).get("types")).getString(0);
+					if (country.compareTo("country") == 0) {
+						country = ((JSONObject)addrComp.get(3)).getString("long_name");
+						addr.setCountryName(country);
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				Double lon = Double.valueOf(0);
+				Double lat = Double.valueOf(0);
+
+				try {
+
+					lon = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+						.getJSONObject("geometry").getJSONObject("location")
+						.getDouble("lng");
+
+					lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
+						.getJSONObject("geometry").getJSONObject("location")
+						.getDouble("lat");
+
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				addr.setLatitude(lat);
+				addr.setLongitude(lon);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return addr;
+	}
+	
+	public static void geocode(final Context context, final LocationParams params, final Async.Block<Address> completion) {
+		if (sGeocoder == null) {
+			sGeocoder = new Geocoder(context, Locale.getDefault());
+		}
+		Async.dispatch(new Runnable() {
+			@Override
+			public void run() {
+				List<Address> addresses = null;
+				try {
+					if (params.address == null) {
+						addresses = sGeocoder.getFromLocation(params.lat, params.lng, 1);
+					} else {
+						addresses = new ArrayList<Address>();
+						addresses.add(getLocationInfo(params.address));
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				final Address address = addresses != null && addresses.size() > 0 ? addresses.get(0) : null;
+				Async.dispatchMain(new Runnable() {
+					@Override
+					public void run() {
+						if (completion != null) {
+							if (address != null  && !(address.getLatitude() == 0 && address.getLongitude() == 0)) {
+								completion.call(address);
+							} else {
+								completion.call(null);
+							}
+						}
+					}
+				});
+			}
+		});
 	}
 }
